ce4e52b20b56c8c3d14f52f7502334362503253f
chan <xrq@89mc.com>
2017-02-07 08:53:03 +0800

    curd hdfs file

--- hbe/src/main/java/me/dev/hbe/controller/IndexController.java
+++ hbe/src/main/java/me/dev/hbe/controller/IndexController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,17 +19,19 @@ import java.util.Map;
 @Controller
 public class IndexController {
 
+    private String fs_default_name = "hdfs://192.168.1.11:9000";
+
     @RequestMapping("upload_file") @ResponseBody
     public Map<String,Object> uploadFileToHdfs(@RequestParam Map<String,Object> map) throws Exception{
         // 上传文件到hdfs系统。。
         // hadoop课件的 云存储系统4 的6：54位置
-        Configuration conf = new Configuration();
-        conf.set("fs.default.name","hdfs://192.168.1.6:9000");
-        FileSystem hdfs = FileSystem.get(conf);
+        FileSystem hdfs = getFS();
         Path src = new Path("F:\\hdfs_test.log")
                 , dest = new Path("/data/hdfs_test.log")
                 ;
+        LogUtil.info("----start copy file ---");
         hdfs.copyFromLocalFile(src, dest);
+        LogUtil.info("----end copy file ---");
         map.put("ret","success");
         return map;
     }
@@ -36,21 +39,45 @@ public class IndexController {
     @RequestMapping("get_file") @ResponseBody
     public Map<String,Object> get_file(@RequestParam Map<String,Object> map) throws Exception{
         // 上传文件到hdfs系统。。
-        // hadoop课件的 云存储系统4 的6：54位置
-        Configuration conf = new Configuration();
-        conf.set("fs.default.name","hdfs://192.168.1.6:9000");
-        FileSystem hdfs = FileSystem.get(conf);
-        Path src = new Path("D:\\hdfs_test.log")
-                , dest = new Path("/data/hdfs_test.log")
+        // hadoop课件的 云存储系统4 的19：13位置
+        FileSystem hdfs = getFS();
+        Path dest = new Path("D:\\hdfs_test.log")
+                , src = new Path("/data/hdfs_test.log")
                 ;
-//        LogUtil.info("----start copy file ---");
-//        hdfs.copyFromLocalFile(source, dest);
-
-        hdfs.copyToLocalFile(dest, src );
-
-//        LogUtil.info("----end copy file ---");
+        LogUtil.info("----start get file ---");
+        hdfs.copyToLocalFile(src, dest );
+        LogUtil.info("----end get file ---");
         map.put("ret","success");
         return map;
+    }
+
+    @RequestMapping("delete_file") @ResponseBody
+    public Map<String,Object> delete_file(@RequestParam Map<String,Object> map) throws Exception{
+        // 上传文件到hdfs系统。。
+        // hadoop课件的 云存储系统4 的14：39位置
+        FileSystem hdfs = getFS();
+        Path dest = new Path("/data/hdfs_test.log")
+                ;
+        LogUtil.info("----start delete file ---");
+        hdfs.delete(dest, false );
+        LogUtil.info("----end delete file ---");
+        map.put("ret","success");
+        return map;
+    }
+
+    private FileSystem getFS() {
+        Configuration conf = new Configuration();
+        conf.set("fs.default.name",fs_default_name);
+        FileSystem hdfs = null;
+        String HADOOP_HOME = "E:\\big_data\\hadoop-3.0.0-alpha1";
+        try {
+            System.setProperty("hadoop.home.dir",HADOOP_HOME);
+            System.setProperty("HADOOP_HOME", HADOOP_HOME);
+            hdfs = FileSystem.get(conf);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return hdfs;
     }
 
 
